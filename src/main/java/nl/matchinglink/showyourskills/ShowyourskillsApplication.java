0454153e5dc9942879a14ab8844353ada6a03679
Marco <marco@ohlalatracks.com>
2018-07-23 09:19:57 +0200

    initial commit

---
+++ src/main/java/nl/matchinglink/showyourskills/ShowyourskillsApplication.java
@@ -0,0 +1,12 @@
+package nl.matchinglink.showyourskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowyourskillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowyourskillsApplication.class, args);
+	}
+}
